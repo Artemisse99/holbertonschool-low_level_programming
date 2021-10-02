e844ad1bdadfee10327c186b3929b68ee4a04f70
Artemisse0921 <egonzalesp@unicesar.edu.co>
2021-10-02 10:54:52 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print of the number 0-9
+ * Return: alwais 0
+ */
+
+int main(void)
+{
+
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		printf(c);
+	}
+	printf('\n');
+	return (0);
+}
