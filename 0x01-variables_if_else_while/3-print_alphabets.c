0664d778cc78d28ff9362a84daa9d68499420afc
Artemisse0921 <egonzalesp@unicesar.edu.co>
2021-10-02 10:02:25 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print of the alfabet lowercase and uppercase
+ * return: alwais 0
+ */
+
+int main(void)
+{
+
+	char c;
+
+	for (c = 'a'; c = 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (j = 'A'; j = 'Z'; j++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
