c2be900433a46417e0b2cd0b19d44d044e75e0cc
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-10-05 11:14:09 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - alphabet lowercase x10
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 1; j <= 10; j++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
