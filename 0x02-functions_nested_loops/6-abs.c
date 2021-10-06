0f2d855c07c5b91f209a06bead91048b7a7e310b
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-10-05 21:38:45 -0700

    Sign

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - print if lowercase
+ *
+ * @x: The character to print
+ *
+ * Return: using two
+ */
+int _abs(int x)
+{
+
+	if (x > 0)
+	{
+	return (x);
+	}
+	else
+	{
+		return (x * -1);
+	}
+	_putchar('\n');
+}
