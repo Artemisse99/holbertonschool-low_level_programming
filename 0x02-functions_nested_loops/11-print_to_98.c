d317abc76b2ffc8978e650ff0430d792c688bb27
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-10-08 17:27:30 -0500

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,13 +9,27 @@
 
 void print_to_98(int n)
 {
-int i;
-for (i = 0; i <= n ; i++)
+int b;
+if (n > 98)
 {
-if (i <= 98)
+for (b = n; b > 97; b--)
 {
-printf("%d", i);
-printf(" ");
+printf("%d", b);
+if (b != 98)
+{
+printf(", ");
+}
+}
+}
+else
+{
+for (b = n; b < 99; b++)
+{
+printf("%d", b);
+if (b != 98)
+{
+printf(", ");
+}
 }
 }
 printf("\n");
