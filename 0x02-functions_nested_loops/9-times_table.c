edeff11617736b9d32374717459633024d3ab669
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-10-08 15:17:58 -0500

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,36 +10,37 @@
 
 void times_table(void)
 {
-  int i, a, c = 0;
+int i = 0;
+int a = 0;
+int c = 0;
 
-  for (i = 0; i <= 9; i++)
-    {
-      for (a = 0; a <= 9; a++)
-	{
-	  c = i * a;
+for (i = 0; i <= 9; i++)
+{
+for (a = 0; a <= 9; a++)
+{
+c = i * a;
+if (a == 0)
+{
+_putchar(c + '0');
+}
 
-	  if (a == 0)
-	    {
-	      _putchar(c + '0');
-	    }
+else if (c <= 9)
 
-	  else if (c <= 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(c + '0');
+}
 
-	    {
-	      _putchar(',');
-	      _putchar(' ');
-	      _putchar(' ');
-	      _putchar(c + '0');
-	    }
-
-	  else if (c >= 10)
-	    {
-	      _putchar(',');
-	      _putchar(' ');
-	      _putchar((c / 10) + '0');
-	      _putchar(c % 10 + '0');
-	    }
-	}
-      _putchar('\n');
-    }
+else if (c >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((c / 10) + '0');
+_putchar(c % 10 + '0');
+}
+}
+_putchar('\n');
+}
 }
