90b09a4ef09040775bc05981799aedb5da7aafa1
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-10-08 14:35:43 -0500

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * times_table - Entry Point
+ * i: I
+ * a: A
+ * c: C
+ * Return: table of the 9
+ */
+
+void times_table(void)
+{
+  int i, a, c = 0;
+
+  for (i = 0; i <= 9; i++)
+    {
+      for (a = 0; a <= 9; a++)
+	{
+	  c = i * a;
+
+	  if (a == 0)
+	    {
+	      _putchar(c + '0');
+	    }
+
+	  else if (c <= 9)
+
+	    {
+	      _putchar(',');
+	      _putchar(' ');
+	      _putchar(' ');
+	      _putchar(c + '0');
+	    }
+
+	  else if (c >= 10)
+	    {
+	      _putchar(',');
+	      _putchar(' ');
+	      _putchar((c / 10) + '0');
+	      _putchar(c % 10 + '0');
+	    }
+	}
+      _putchar('\n');
+    }
+}
