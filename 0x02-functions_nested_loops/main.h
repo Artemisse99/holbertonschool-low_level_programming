87fd967a5b7f269955c614d658fe469c0463deca
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-10-08 14:36:48 -0500

    file main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+char _putchar();
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -6,4 +7,4 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-char _putchar();
+void times_table(void);
