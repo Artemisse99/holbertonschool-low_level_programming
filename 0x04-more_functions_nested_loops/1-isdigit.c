263c955b9d6db0e9d987a7732ff39cddaa163ed6
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-10-09 18:03:58 -0500

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isdigit - print if _isdigit
+ *
+ * @c: The character to print
+ *
+ * Return: using two
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+
+	else
+
+	{
+		return (0);
+	}
+}
