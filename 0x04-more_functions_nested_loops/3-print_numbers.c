90809ecc165ade4121a6a03bbf6145376fb13545
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-10-09 19:38:09 -0500

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print of the number 0-9
+ * Return: alwais 0
+ */
+
+void print_numbers(void)
+{
+
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+}
