c38caed70249f1b680b3c2c747a7b65d61fd2dbf
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-10-11 18:32:51 -0500

    Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - using first time pointers- update variable
+ *@a: Pointers
+ *@b: Pointers
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
