5270d766ee4b3ef6d045aa827f28314d05a366ea
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-10-14 12:14:57 -0500

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
- * _strcat - using first time pointers- concat string
+ * _strncat - using first time pointers- concat string
  *@src: Pointers
  *@dest: Parameters pointers
- *Return: dest
+ *@n: Parameters quantity of bytes
+*Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	char *concat = dest;
 	int c = 0;
+
 	while (*concat != '\0')
 	{
 		concat++;
@@ -19,15 +21,17 @@ char *_strncat(char *dest, char *src, int n)
 		concat++;
 		src++;
 	}
-	while (n > c )
-	   n = c;
-	while ((n > 0 ) && (*src != '\0'))
+	while (n > c)
+	{
+		n = c;
+	}
+	while ((n > 0) && (*src != '\0'))
 	{
 		*concat = *src;
 		n++;
 	}
 
-	*concat= '\0';
+	*concat = '\0';
 
 	return (dest);
 
