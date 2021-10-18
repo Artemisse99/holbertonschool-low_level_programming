d87e780a789cf480efae26d479673c1624bdd3e6
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-10-18 14:24:10 -0500

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_strchr - kjkjl
+ * @c: Parameters
+ * @s: Parameters
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			s = &s[i];
+			return (s);
+		}
+		
+
+	}
+	return (NULL);
+}
