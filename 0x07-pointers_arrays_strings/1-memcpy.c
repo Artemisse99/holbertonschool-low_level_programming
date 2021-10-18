2307bdc1742835dd4049a53cc6238c7435c00959
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-10-18 15:09:27 -0500

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,18 +4,15 @@
  * @dest: Parameters
  * @src: Parameters
  * @n: Parameters
- * Return: 0
+ * Return: pointer
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-
 	unsigned int i;
 
-	for(i = 0; i < n ; i++)
-	{
+	for (i = 0; i < n ; i++)
 		dest[i] = src[i];
-       	}
 
 	return (dest);
 }
