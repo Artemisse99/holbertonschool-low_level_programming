a99262cc6367a849ee2398a066cae6be2802b5d1
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-10-18 12:05:52 -0500

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_memset - kjkjl
+ * @s: Parameters
+ * @b: Parameters
+ * @n: Parameters
+ * Return: 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+}
