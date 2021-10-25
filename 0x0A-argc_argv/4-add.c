e2549dd308634fd2700e04c1673d5042b67d0443
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-10-25 18:55:26 -0500

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *main - prints the number of arguments passed into it
+ * @argv: Parameters
+ * @argc: Parameters
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	unsigned int k, sum = 0;
+	char *e;
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			e = argv[i];
+			for (k = 0; k < strlen(e); k++)
+			{
+				if (e[k] < 48 || e[k] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(e);
+			e++;
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
