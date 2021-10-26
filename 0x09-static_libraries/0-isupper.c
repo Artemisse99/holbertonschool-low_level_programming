92d59c093999ca79cf364bae480103597cc77ef3
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-10-26 15:39:54 -0500

    Static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - print if uppercase
+ *
+ * @c: The character to print
+ *
+ * Return: using two
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+	else
+
+	{
+		return (0);
+	}
+}
