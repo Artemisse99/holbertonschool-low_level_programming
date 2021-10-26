66cb397139b26f2b654b368f2b972b2922a132ba
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-10-25 19:28:10 -0500

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,11 +12,13 @@ int main(int argc, char *argv[])
 	int i;
 	unsigned int k, sum = 0;
 	char *e;
+
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
 			e = argv[i];
+
 			for (k = 0; k < strlen(e); k++)
 			{
 				if (e[k] < 48 || e[k] > 57)
@@ -25,14 +27,17 @@ int main(int argc, char *argv[])
 					return (1);
 				}
 			}
+
 			sum += atoi(e);
 			e++;
 		}
+
 		printf("%d\n", sum);
 	}
 	else
 	{
 		printf("0\n");
 	}
+
 	return (0);
 }
