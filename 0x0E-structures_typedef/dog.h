e2cd0f6b84ff9b5f20df9e85f91951c3cddea0d7
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-11-01 18:27:19 -0500

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,13 @@
  *@age:variable
  * Description: Longer description
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
