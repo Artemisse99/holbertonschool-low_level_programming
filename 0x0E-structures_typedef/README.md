20e3511576144a08c7f445a9c9f57f366d7bba2c
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-11-01 18:16:56 -0500

    0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+Hello
+This is the directory 0x0E. C - Structures, typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Short description
+ *@name: variable
+ *@owner:variable
+ *@age:variable
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
