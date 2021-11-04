c2456751158b0ca1c7c1d3795f6bea7a52f270a0
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-11-04 18:22:49 -0500

    If you spend too much time thinking about a thing, you'll never get it done

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+/**
+ * array_iterator - search integer
+ * @array: array
+ * @size: size array
+ * @action: pointer to function
+ *
+ * Return: integer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (size != 0 && array != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
