87d23da29c7031ac03988714bb81f66764dd5bff
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-11-05 17:31:29 -0500

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,31 +1,23 @@
-#include "3-calc.h"
-#include <string.h>
+#ifndef CALC_H
+#define CALC_H
+
+int (*get_op_func(char *s))(int, int);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 /**
- *get_op_func - Selects correct function to perform an operation asked by user.
- *@s: selects a correct function to perform the operation asked by user.
- *Return: Pointer to function that corresponds to operator given as parameter.
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
  */
-int (*get_op_func(char *s))(int, int)
+typedef struct op
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i;
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
 
-	i = 0;
-
-	while (ops[i].op != '\0')
-	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);
-
-		i++;
-	}
-
-	return (ops[i].f);
-}
+#endif /* CALC_H */
