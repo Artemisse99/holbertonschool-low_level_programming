f68f73af5d5857f4d3989ad7e3543bac0d3106cd
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-11-13 01:47:06 -0500

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,9 +9,9 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
-		if(h->str == NULL)
+		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 
@@ -21,7 +21,7 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", h->len, h->str);
 
 		}
-		count ++;
+		count++;
 		h = h->next;
 
 	}
