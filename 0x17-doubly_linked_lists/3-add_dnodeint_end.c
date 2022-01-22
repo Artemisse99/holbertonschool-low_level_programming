f99199d71964b64396df3ef4a91a3e7d61393732
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-01-22 11:02:50 -0500

    Add node at the end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - add node at end
+ * @head: parameter double pointer
+ * @n: parameter int
+ * Return: new node
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *nuevo_nodo = NULL;
+	dlistint_t *tmp = *head;
+
+	if (!head)
+		return (NULL);
+
+	new_node = createNode(n);
+	if (!new_node)
+		return (NULL);
+	if (!*head)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	while (Next(tmp))
+		tmp = Next(tmp);
+	insertAfter(new_node, tmp);
+	return (new_node);
+}
