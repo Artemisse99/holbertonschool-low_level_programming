fabb5593721409c0b10e7b4036b57eab82943b1f
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-01-22 10:45:51 -0500

    functions add

--- 0x17-doubly_linked_lists/head.c
+++ 0x17-doubly_linked_lists/head.c
@@ -15,3 +15,36 @@ void printNode(const dlistint_t *L)
 {
 	printf("%d\n", L->n);
 }
+dlistint_t *before(const dlistint_t *L)
+{
+	return (L->prev);
+}
+void insertBefore(dlistint_t *v_insert, dlistint_t *site)
+{
+	v_insert->next = site;
+	if (thereAre(before(site)))
+		site->prev->next = v_insert;
+	v_insert->prev = before(site);
+	site->prev = v_insert;
+}
+
+void insertAfter(dlistint_t *v_insert, dlistint_t *site)
+{
+	v_insert->prev = site;
+	v_insert->next = Next(site);
+	if (thereAre(Next(site)))
+		site->next->prev = v_insert;
+	site->next = v_insert;
+}
+dlistint_t *createNode(const int n)
+{
+	dlistint_t *node = malloc(sizeof(dlistint_t));
+
+	if (!node)
+		return (NULL);
+	node->n = n;
+	node->prev = NULL;
+	node->next = NULL;
+
+	return (node);
+}
